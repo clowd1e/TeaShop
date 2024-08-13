9b0bbefbb64bae568700247452ecd618b4624354
Oleksandr Lobchenko <61800741+clowd1e@users.noreply.github.com>
2024-08-13 16:03:34 +0200

    Add Identity errors

---
+++ TeaShop.API/TeaShop.Application/ResultBehavior/Errors/Identity/LoginErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace TeaShop.Application.ResultBehavior.Errors.Identity
+{
+    public static class LoginErrors
+    {
+        public static readonly Error EmailNotFound = new("Login.EmailNotFound", "Email address not found.");
+        public static readonly Error InvalidPassword = new("Login.InvalidPassword", "Invalid password.");
+    }
+}
---
+++ TeaShop.API/TeaShop.Application/ResultBehavior/Errors/Identity/RegisterErrors.cs
@@ -0,0 +1,8 @@
+﻿namespace TeaShop.Application.ResultBehavior.Errors.Identity
+{
+    public sealed class RegisterErrors
+    {
+        public static Error EmailAlreadyExists => new("Register.EmailAlreadyExists", "Email already exists.");
+        public static Error UserCreationFailed => new("Register.UserCreationFailed", "User creation failed.");
+    }
+}
